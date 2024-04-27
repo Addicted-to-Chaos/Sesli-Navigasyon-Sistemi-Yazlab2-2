509b1f2a12e30b539bda8531bc8e1f32186c1fc9
Edaobz <edaobuz4@gmail.com>
2024-04-28 01:03:09 +0300

    düzenleme

---
+++ YazlabNav/app/src/main/java/com/example/yazlabnav/GirisEkrani.kt
@@ -0,0 +1,20 @@
+package com.example.yazlabnav
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class GirisEkrani : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_giris_ekrani)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
